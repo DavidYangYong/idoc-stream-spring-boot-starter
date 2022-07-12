6519000c2e7928af7a17c654227ef6f4c76d76a7
DavidYangYong <245317989@qq.com>
2022-07-12 16:53:16 +0800

    feat: change code write mode

--- src/main/java/com/fl/idoc/stream/springbootstarter/listener/RuleProperties.java
+++ src/main/java/com/fl/idoc/stream/springbootstarter/listener/RuleProperties.java
@@ -2,13 +2,11 @@ package com.fl.idoc.stream.springbootstarter.listener;
 
 import java.util.List;
 import org.springframework.boot.context.properties.ConfigurationProperties;
-import org.springframework.stereotype.Component;
 
 /**
  * @author david
  * @date 2021-01-08 10:10
  **/
-@Component
 @ConfigurationProperties(prefix = "fl.cloud.idoc.stream")
 public class RuleProperties {
 
