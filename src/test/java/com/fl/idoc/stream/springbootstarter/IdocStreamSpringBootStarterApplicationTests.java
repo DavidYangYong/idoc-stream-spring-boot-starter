dcc4c4343df133002c7ddb9d7a36906505f42613
DavidYangYong <yyzhpq@163.com>
2023-01-17 15:49:47 +0800

    feat(service): remove unused code-04

--- src/test/java/com/fl/idoc/stream/springbootstarter/IdocStreamSpringBootStarterApplicationTests.java
+++ src/test/java/com/fl/idoc/stream/springbootstarter/IdocStreamSpringBootStarterApplicationTests.java
@@ -3,12 +3,11 @@ package com.fl.idoc.stream.springbootstarter;
 import com.fl.idoc.stream.springbootstarter.annotation.IdocStreamScan;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 @RunWith(SpringJUnit4ClassRunner.class)
-@SpringBootTest(classes = {IdocStreamSpringBootStarterApplication.class})
+//@SpringBootTest(classes = {IdocStreamSpringBootStarterApplication.class})
 @ActiveProfiles("test")
 @IdocStreamScan(basePackages = "com.fl.idoc.stream.springbootstarter.service")
 public class IdocStreamSpringBootStarterApplicationTests {
