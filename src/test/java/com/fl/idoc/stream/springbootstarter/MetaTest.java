aaed4bb5f1b43edefc5e75fabf6d5328a1173ceb
DavidYangYong <yyzhpq@163.com>
2023-01-18 13:53:54 +0800

    feat(service): add optional is true

--- src/test/java/com/fl/idoc/stream/springbootstarter/MetaTest.java
+++ src/test/java/com/fl/idoc/stream/springbootstarter/MetaTest.java
@@ -7,6 +7,7 @@ import java.util.Map;
 import java.util.Map.Entry;
 import java.util.Optional;
 import java.util.function.Predicate;
+import lombok.extern.slf4j.Slf4j;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -25,6 +26,7 @@ import org.springframework.util.StringUtils;
  * @Description:
  **/
 @RunWith(SpringJUnit4ClassRunner.class)
+@Slf4j
 public class MetaTest {
 
 	@Test
@@ -34,7 +36,7 @@ public class MetaTest {
 		OrdntfBaseServiceImpl ordntfBaseService = new OrdntfBaseServiceImpl();
 		handlerMap.put("ordntfBaseServiceImpl", ordntfBaseService);
 		String mesType = handlerMap.get("ordntfBaseServiceImpl").getMesType();
-		System.out.println(mesType);
+		log.info(mesType);
 		Optional<IBaseExecService> optionalIBaseExecService = handlerMap.entrySet().stream()
 				.filter(new Predicate<Entry<String, IBaseExecService>>() {
 					@Override
@@ -43,50 +45,50 @@ public class MetaTest {
 								+ ":ORDINT01");
 					}
 				}).map(Map.Entry::getValue).findFirst();
-		System.out.println(optionalIBaseExecService.get());
-		System.out.println("==============ClassMetadata==============");
+		log.info("{}", optionalIBaseExecService.get());
+		log.info("==============ClassMetadata==============");
 		ClassMetadata classMetadata = metadata;
-		System.out.println(classMetadata.getClassName());
-		System.out.println(classMetadata.getEnclosingClassName());
-		System.out.println(StringUtils.arrayToCommaDelimitedString(classMetadata.getMemberClassNames()));
-		System.out.println(StringUtils.arrayToCommaDelimitedString(classMetadata.getInterfaceNames()));
-		System.out.println(classMetadata.hasSuperClass());
-		System.out.println(classMetadata.getSuperClassName());
+		log.info(classMetadata.getClassName());
+		log.info(classMetadata.getEnclosingClassName());
+		log.info(StringUtils.arrayToCommaDelimitedString(classMetadata.getMemberClassNames()));
+		log.info(StringUtils.arrayToCommaDelimitedString(classMetadata.getInterfaceNames()));
+		log.info("{}", classMetadata.hasSuperClass());
+		log.info(classMetadata.getSuperClassName());
 
-		System.out.println(classMetadata.isAnnotation());
-		System.out.println(classMetadata.isFinal());
-		System.out.println(classMetadata.isIndependent());
+		log.info("{}", classMetadata.isAnnotation());
+		log.info("{}", classMetadata.isFinal());
+		log.info("{}", classMetadata.isIndependent());
 
-		System.out.println("==============AnnotatedTypeMetadata==============");
+		log.info("==============AnnotatedTypeMetadata==============");
 		AnnotatedTypeMetadata annotatedTypeMetadata = metadata;
-		System.out.println(annotatedTypeMetadata.isAnnotated(HandlerType.class.getName()));
-		System.out.println(annotatedTypeMetadata.isAnnotated(Component.class.getName()));
+		log.info("{}", annotatedTypeMetadata.isAnnotated(HandlerType.class.getName()));
+		log.info("{}", annotatedTypeMetadata.isAnnotated(Component.class.getName()));
 
-		System.out.println(annotatedTypeMetadata.getAnnotationAttributes(HandlerType.class.getName()));
-		System.out.println(annotatedTypeMetadata.getAnnotationAttributes(Component.class.getName()));
-		System.out.println(annotatedTypeMetadata.getAnnotationAttributes(EnableAsync.class.getName()));
+		log.info("{}", annotatedTypeMetadata.getAnnotationAttributes(HandlerType.class.getName()));
+		log.info("{}", annotatedTypeMetadata.getAnnotationAttributes(Component.class.getName()));
+		log.info("{}", annotatedTypeMetadata.getAnnotationAttributes(EnableAsync.class.getName()));
 
-		System.out.println(annotatedTypeMetadata.getAllAnnotationAttributes(HandlerType.class.getName()));
-		System.out.println(annotatedTypeMetadata.getAllAnnotationAttributes(Component.class.getName()));
-		System.out.println(annotatedTypeMetadata.getAllAnnotationAttributes(EnableAsync.class.getName()));
+		log.info("{}", annotatedTypeMetadata.getAllAnnotationAttributes(HandlerType.class.getName()));
+		log.info("{}", annotatedTypeMetadata.getAllAnnotationAttributes(Component.class.getName()));
+		log.info("{}", annotatedTypeMetadata.getAllAnnotationAttributes(EnableAsync.class.getName()));
 
-		System.out.println("==============AnnotationMetadata==============");
+		log.info("==============AnnotationMetadata==============");
 		AnnotationMetadata annotationMetadata = metadata;
-		System.out.println(annotationMetadata.getAnnotationTypes());
-		System.out.println(annotationMetadata.getMetaAnnotationTypes(HandlerType.class.getName()));
-		System.out.println(annotationMetadata.getMetaAnnotationTypes(Component.class.getName()));
+		log.info("{}", annotationMetadata.getAnnotationTypes());
+		log.info("{}", annotationMetadata.getMetaAnnotationTypes(HandlerType.class.getName()));
+		log.info("{}", annotationMetadata.getMetaAnnotationTypes(Component.class.getName()));
 
-		System.out.println(annotationMetadata.hasAnnotation(HandlerType.class.getName()));
-		System.out.println(annotationMetadata.hasAnnotation(Component.class.getName()));
+		log.info("{}", annotationMetadata.hasAnnotation(HandlerType.class.getName()));
+		log.info("{}", annotationMetadata.hasAnnotation(Component.class.getName()));
 
-		System.out.println(annotationMetadata.hasMetaAnnotation(HandlerType.class.getName()));
-		System.out.println(annotationMetadata.hasMetaAnnotation(Component.class.getName()));
+		log.info("{}", annotationMetadata.hasMetaAnnotation(HandlerType.class.getName()));
+		log.info("{}", annotationMetadata.hasMetaAnnotation(Component.class.getName()));
 
-		System.out.println(annotationMetadata.hasAnnotatedMethods(Autowired.class.getName()));
+		log.info("{}", annotationMetadata.hasAnnotatedMethods(Autowired.class.getName()));
 		annotationMetadata.getAnnotatedMethods(Autowired.class.getName()).forEach(methodMetadata -> {
-			System.out.println(methodMetadata.getClass());
-			System.out.println(methodMetadata.getMethodName());
-			System.out.println(methodMetadata.getReturnTypeName());
+			log.info("{}", methodMetadata.getClass());
+			log.info(methodMetadata.getMethodName());
+			log.info(methodMetadata.getReturnTypeName());
 		});
 	}
 }
