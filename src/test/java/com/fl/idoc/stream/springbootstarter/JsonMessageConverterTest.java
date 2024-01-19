4ccacc4fd4da6e1f470c55a30fc46ed714c4695b
DavidYangYong <yyzhpq@163.com>
2024-01-19 08:43:47 +0800

    feat(component): add List

---
+++ src/test/java/com/fl/idoc/stream/springbootstarter/JsonMessageConverterTest.java
@@ -0,0 +1,20 @@
+package com.fl.idoc.stream.springbootstarter;
+
+
+import org.springframework.amqp.core.Message;
+import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
+import org.springframework.amqp.support.converter.MessageConverter;
+
+/**
+ * @author david
+ * @create 2024-01-19 08:27
+ * @Description:
+ **/
+public class JsonMessageConverterTest {
+
+	public static void main(String[] args) {
+		MessageConverter messageConverter = new Jackson2JsonMessageConverter();
+		Message message = null;
+		messageConverter.fromMessage(message);
+	}
+}
