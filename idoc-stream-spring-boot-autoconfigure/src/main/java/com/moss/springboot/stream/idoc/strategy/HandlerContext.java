0f55ca604cb7aa3de1e7d6043d6fc9017bd9d374
DavidYangYong <yyzhpq@163.com>
2024-06-28 19:50:14 +0800

    Revert "feat(service): new task write mode"
    This reverts commit 8517b6dca9b3948240edebab2cdef4a77ba829af.

--- idoc-stream-spring-boot-autoconfigure/src/main/java/com/moss/springboot/stream/idoc/strategy/HandlerContext.java
+++ idoc-stream-spring-boot-autoconfigure/src/main/java/com/moss/springboot/stream/idoc/strategy/HandlerContext.java
@@ -1,7 +1,6 @@
 package com.moss.springboot.stream.idoc.strategy;
 
 import com.moss.springboot.stream.idoc.service.base.IBaseExecService;
-import com.moss.springboot.stream.idoc.service.base.IBaseTaskService;
 import java.util.List;
 import java.util.Optional;
 import org.apache.commons.collections4.CollectionUtils;
@@ -14,31 +13,17 @@ import org.apache.commons.lang3.StringUtils;
 public class HandlerContext {
 
 	private List<IBaseExecService> baseExecServiceList;
-	private List<IBaseTaskService> baseTaskServiceList;
 
-	public HandlerContext(List<IBaseExecService> baseExecServiceList,
-			List<IBaseTaskService> baseTaskServiceList) {
+	public HandlerContext(List<IBaseExecService> baseExecServiceList) {
 		this.baseExecServiceList = baseExecServiceList;
-		this.baseTaskServiceList = baseTaskServiceList;
 	}
 
-	public IBaseExecService getInstanceExecService(String mesTyp) {
+	public IBaseExecService getInstance(String mesTyp) {
 		if (CollectionUtils.isEmpty(baseExecServiceList)) {
 			return null;
 		}
 		Optional<IBaseExecService> baseExecServiceOptional = baseExecServiceList.stream()
 				.filter(iBaseExecService -> StringUtils.equals(iBaseExecService.getMesType(), mesTyp)).findFirst();
-
-		return baseExecServiceOptional.orElse(null);
-	}
-
-	public IBaseTaskService getInstanceTaskService(String mesTyp) {
-		if (CollectionUtils.isEmpty(baseTaskServiceList)) {
-			return null;
-		}
-		Optional<IBaseTaskService> baseExecServiceOptional = baseTaskServiceList.stream()
-				.filter(iBaseTaskService -> StringUtils.equals(iBaseTaskService.getMesType(), mesTyp))
-				.findFirst();
 
 		return baseExecServiceOptional.orElse(null);
 	}
