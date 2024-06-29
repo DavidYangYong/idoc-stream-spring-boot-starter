4c79a81a692ef237cd34200ee04f6c69e5d1ca73
DavidYangYong <yyzhpq@163.com>
2024-06-29 11:47:45 +0800

    feat(service): new task write mode

--- idoc-stream-spring-boot-autoconfigure/src/main/java/com/moss/springboot/stream/idoc/autoconfigure/IdocSinkAutoConfiguration.java
+++ idoc-stream-spring-boot-autoconfigure/src/main/java/com/moss/springboot/stream/idoc/autoconfigure/IdocSinkAutoConfiguration.java
@@ -1,9 +1,12 @@
 package com.moss.springboot.stream.idoc.autoconfigure;
 
 import com.moss.springboot.stream.idoc.listener.IdocListener;
+import com.moss.springboot.stream.idoc.service.base.IBaseTaskService;
 import java.util.function.Function;
+import java.util.function.Supplier;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.lang3.StringUtils;
+import org.springframework.beans.factory.ObjectProvider;
 import org.springframework.boot.autoconfigure.AutoConfigureAfter;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnBean;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
@@ -23,27 +26,36 @@ import org.springframework.messaging.support.MessageBuilder;
 @ConditionalOnBean({IdocListener.class})
 public class IdocSinkAutoConfiguration {
 
-	private final IdocListener idocListener;
+  private final IdocListener idocListener;
+
+  private IBaseTaskService baseTaskService;
+
+  public IdocSinkAutoConfiguration(IdocListener idocListener,
+      ObjectProvider<IBaseTaskService> baseTaskService) {
+    this.idocListener = idocListener;
+    this.baseTaskService = baseTaskService.getIfAvailable();
+  }
 
 
-	public IdocSinkAutoConfiguration(IdocListener idocListener) {
-		this.idocListener = idocListener;
-	}
+  @Bean
+  public Supplier<String> idocSupplier() {
+    return () -> baseTaskService.getMessage();
+  }
 
-	@Bean
-	public Function<Message<String>, Message<String>> idocSink() {
-		return msg -> {
-			String payload = msg.getPayload();
-			log.info("idocSink Received payload: {}", payload);
-			MessageHeaders messageHeaders = msg.getHeaders();
-			log.info("idocSink Received messageHeaders: {}", messageHeaders);
-			String payloadTemp;
-			payloadTemp = idocListener.process(payload);
-			if (StringUtils.isNotEmpty(payloadTemp)) {
-				return MessageBuilder.createMessage(payloadTemp, messageHeaders);
-			} else {
-				return null;
-			}
-		};
-	}
+  @Bean
+  public Function<Message<String>, Message<String>> idocSink() {
+    return msg -> {
+      String payload = msg.getPayload();
+      log.info("idocSink Received payload: {}", payload);
+      MessageHeaders messageHeaders = msg.getHeaders();
+      log.info("idocSink Received messageHeaders: {}", messageHeaders);
+      String payloadTemp;
+      payloadTemp = idocListener.process(payload);
+      if (StringUtils.isNotEmpty(payloadTemp)) {
+        return MessageBuilder.createMessage(payloadTemp, messageHeaders);
+      } else {
+        return null;
+      }
+    };
+  }
 }
